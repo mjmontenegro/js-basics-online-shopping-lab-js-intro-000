b71ee5189a0df008d07dc3353aaee0504fa6d55b
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-14 00:14:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  for(let i = 0; i < cart.length, i++) {
+  for(let i = 0; i < cart.length; i++) {
     if(cart[i].itemName === item) {
       cart.splice(i,1);
       return cart;
