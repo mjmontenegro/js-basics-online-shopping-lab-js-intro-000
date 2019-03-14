d93fdb3703d0ce797461c23d4a180029f402c6d0
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-14 00:16:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,4 +43,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(!cardNumber) {
+    return `Sorry, we don't have a credit card on file for you.`
+  }
 }
